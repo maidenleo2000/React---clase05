ccb3273c198dcf2044483d7740ba1e08b6d3b08c
Leandro Martello <maidenleo2000@gmail.com>
2026-01-07 15:06:54 -0300

    Configurado Prettier

--- src/HOC.jsx
+++ src/HOC.jsx
@@ -1,19 +1,21 @@
 //HOC - Higher Order Component => crea un nuevo componente recibiendo un componente
 const vista =
-  (Componente) =>
-  ({ ...props }) => {
-    return (
-      <div className="mb-3">
-        {/* <header className="bg-indigo-500">
+  // eslint-disable-next-line no-unused-vars
+    (Componente) =>
+    // eslint-disable-next-line no-unused-vars
+    ({ ...props }) => {
+      return (
+        <div className="mb-3">
+          {/* <header className="bg-indigo-500">
           <h1>Soy el header</h1>
         </header>
         <Componente /> */}
-        <header className="bg-indigo-500 text-red-800 first:mt-3">
-          <Componente />
-        </header>
-      </div>
-    );
-  };
+          <header className="bg-indigo-500 text-red-800 first:mt-3">
+            <Componente />
+          </header>
+        </div>
+      );
+    };
 
 const ComponenteInicio = () => {
   return <h2 className="text-3xl">Soy vista Inicio</h2>;
